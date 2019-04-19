1643346a7b86227437a4e5cda1ee29893ae16f3c
Mike De Groot <mdegroot09@gmail.com>
2019-04-19 10:17:59 -0600

    added server and database connection

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,10 @@
 module.exports = {
-  
+  getAll(req, res){
+    const db = req.app.get('db')
+    db.read_products().then(response => {
+      res.send(response)
+    }).catch(err => {
+      console.log('err:', err)
+    })
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
-const port = 4000
+const massive = require('massive')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
 
 const ProductCtrl = require('./controller')
 
 app.use(express.json())
 
-app.listen(port, () => {
-  console.log('Listening on port:', port)
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set('db', dbInstance)
+  app.listen(SERVER_PORT, () => {
+    console.log('Listening to port:', SERVER_PORT)
+  })
+}).catch(err => {
+  console.log('err:', err)
 })
+
+app.get('/api/products', ProductCtrl.getAll);
